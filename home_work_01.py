a8a113b78cfd7034cf58e6a6258345dd16c2ada9
sochic2 <netzzang12@gmail.com>
2019-01-04 17:36:58 +0900

    D03 | 190104 | python function CLI

--- home_work_01.py
+++ home_work_01.py
@@ -1,9 +1,28 @@
-import keyword
-print(keyword.kwlist)
+# import keyword
+# print(keyword.kwlist)
 
-import math
-a = 0.1 * 3
-b = 0.3
-print(math.isclose(a, b))
+# import math
+# a = 0.1 * 3
+# b = 0.3
+# print(math.isclose(a, b))
 
-print(float('3.5'))
+# print(float('3.5'))
+
+# def shortcut(word):
+#     mo = ["a", "e", "i", "o", "u"]
+#     cut_word = []
+#     for i in word:
+#         if i in mo:
+#             continue
+#         else:
+#             cut_word.append(i)
+#     return cut_word
+        
+# print(shortcut("ssafy")) #=> ssfy
+# print(shortcut("goodbye")) #=> gdby
+
+
+some_list = [1, 2, 3, []]
+print(some_list)
+
+
