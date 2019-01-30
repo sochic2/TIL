ab5b2fea55ab81390ac6d71cbb68c92fc88c8762
sochic2 <netzzang12@gmail.com>
2019-01-30 13:02:15 +0900

    20190130 | bootstrab | AM

--- study/exam_01.py
+++ study/exam_01.py
@@ -1,4 +1,19 @@
 class Circle:
     pi = 3.14
-    def area(r):
-        return self.pi * r**2  
+    def __init__(self, r, x=0, y=0):
+        self.r = r
+        self.x = x
+        self.y = y
+
+    def area(self):
+        return self.r **2 *  Circle.pi
+    def circumference(self):
+        return 2* self.r * Circle.pi
+    def center(self):
+        return self.x, self.y
+    def move(self, x, y):
+        self.x = x
+        self.y = y
+        print(f'{x}, {y}로 이동되었습니다.')
+        return self.center()    
+
