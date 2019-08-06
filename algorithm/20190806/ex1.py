6c8ecf54c34e31ea79a4ac4249f634a49cab0952
sochic2 <netzzang12@gmail.com>
2019-08-06 19:53:14 +0900

    algorithm

---
+++ algorithm/20190806/ex1.py
@@ -0,0 +1,25 @@
+import sys
+import copy
+sys.stdin = open("ex1_input.txt")
+
+def gorot():
+    while rotation:
+        ind = rotation.pop(0)
+        realrot(ind)
+    return
+
+def realrot(arr):
+    flag = [0, 0, 0, 0, 0]
+    for i in range(4):
+        if rotation[i] != rotation[i+1]:
+            flag[i] = 1
+
+
+    return
+
+T = int(input())
+for tc in range(1, T+1):
+    K = int(input())
+    mag = [list(map(int, input().split())) for _ in range(4) ]
+    rotation = [list(map(int, input().split())) for _ in range(K)]
+    gorot()
