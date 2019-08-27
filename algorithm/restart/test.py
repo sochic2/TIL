b49c7a91dad294e235d24222af5db08a9d3c8157
sochic2 <netzzang12@gmail.com>
2019-08-27 21:28:50 +0900

    190827 | film

--- algorithm/restart/test.py
+++ algorithm/restart/test.py
@@ -1,3 +1,25 @@
-a = [[1, 2, 3], [3, 2, 1]]
-a.sort(key=lambda x:x[2])
-print(a)
+import sys
+sys.stdin = open('디저트.txt')
+def check(y, x, yy, xx):
+    result = []
+    dy = [1, 1, -1, -1]
+    dx = [-1, 1, 1, -1]
+    for py in range(yy):
+
+    for px in range(xx):
+
+    for py in range(yy):
+
+    for px in range(xx):
+
+
+T = int(input())
+for tc in range(1, T+1):
+    N = int(input())
+    maparr = [list(map(int, input().split())) for _ in range(N)]
+    mmax = -1
+    for y in range(N):
+        for x in range(N):
+            for yy in range(N-1):
+                for xx in range(N-1):
+                    check(y, x, yy, xx)
--- algorithm/restart/디저트.py
+++ algorithm/restart/디저트.py
@@ -1,18 +1,57 @@
 import sys
 sys.stdin = open('디저트.txt')
 
-def check():
-    global N
+def check(y, x):
+    global mmax
+    resultarray = []
     dy = [1, 1, -1, -1]
     dx = [-1, 1, 1, -1]
+    lrange = parr[0]
+    rrange = parr[1]
+    for i in range(4):
+        if i % 2 :
+            for r in range(1, lrange):
+                y = y + dy[i]
+                x = x + dx[i]
+                if x < 0 or y < 0 or x >= N or y >= N:return
+                if maparr[y][x] in resultarray: return
+                resultarray.append(maparr[y][x])
+        else:
+            for c in range(1, rrange):
+                y = y + dy[i]
+                x = x + dx[i]
+                if x < 0 or y < 0 or x >= N or y >= N: return
+                if maparr[y][x] in resultarray: return
+                resultarray.append(maparr[y][x])
+    if mmax < len(resultarray):
+        mmax = len(resultarray)
 
+
+def perm(n, y, x):
+    if n == 2:
+        flag = 1
+        for z in parr:
+            flag *= z
+        if flag <= mmax:
+            return
+        else:
+            check(y, x)
+    else:
+        for i in range(2, N):
+            parr[n] = i
+            perm(n+1, y, x)
+            parr[n] = 0
 
 
 T = int(input())
 for tc in range(1, T+1):
     N = int(input())
     maparr = [list(map(int, input().split())) for _ in range(N)]
+    mmax = -1
+    parr = [0] * 2
     for y in range(N):
         for x in range(N):
-            visit = [0] * 100
-            check(y, x, 4)
+            perm(0, y, x)
+
+    print('#{} {}'.format(tc, mmax))
+
