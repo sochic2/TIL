7fcbee4f1015277eac055785518b607686ab776f
sochic2 <netzzang12@gmail.com>
2019-10-14 17:48:36 +0900

    bridge

---
+++ algorithm/restart/kruskal.py
@@ -0,0 +1,82 @@
+parent = {}
+rank = {}
+
+# 정점을 독립적인 집합으로 만든다.
+def make_set(v):
+    parent[v] = v
+    rank[v] = 0
+
+# 해당 정점의 최상위 정점을 찾는다.
+def find(v):
+    if parent[v] != v:
+        parent[v] = find(parent[v])
+
+    return parent[v]
+
+
+# 두 정점을 연결한다.
+def union(v, u):
+    root1 = find(v)
+    root2 = find(u)
+
+    if root1 != root2:
+        # 짧은 트리의 루트가 긴 트리의 루트를 가리키게 만드는 것이 좋다.
+        if rank[root1] > rank[root2]:
+            parent[root2] = root1
+        else:
+            parent[root1] = root2
+
+            if rank[root1] == rank[root2]:
+                rank[root2] += 1
+
+
+def kruskal(graph):
+    for v in graph['vertices']:
+        make_set(v)
+
+    mst = []
+
+    edges = graph['edges']
+    edges.sort()
+
+    for edge in edges:
+        weight, v, u = edge
+
+        if find(v) != find(u):
+            union(v, u)
+            mst.append(edge)
+
+    return mst
+
+
+
+
+graph = {
+    'vertices': ['A', 'B', 'C', 'D', 'E', 'F', 'G'],
+    'edges': [
+        (7, 'A', 'B'),
+        (5, 'A', 'D'),
+        (7, 'B', 'A'),
+        (8, 'B', 'C'),
+        (9, 'B', 'D'),
+        (7, 'B', 'E'),
+        (8, 'C', 'B'),
+        (5, 'C', 'E'),
+        (5, 'D', 'A'),
+        (9, 'D', 'B'),
+        (7, 'D', 'E'),
+        (6, 'D', 'F'),
+        (7, 'E', 'B'),
+        (5, 'E', 'C'),
+        (15, 'E', 'D'),
+        (8, 'E', 'F'),
+        (9, 'E', 'G'),
+        (6, 'F', 'D'),
+        (8, 'F', 'E'),
+        (11, 'F', 'G'),
+        (9, 'G', 'E'),
+        (11, 'G', 'F'),
+    ]
+}
+
+print(kruskal(graph))
--- algorithm/restart/다리만들기2.py
+++ algorithm/restart/다리만들기2.py
@@ -10,6 +10,45 @@ def island(y, x, cnt):
             narr[ny][nx] = cnt
             island(ny, nx, cnt)
 
+def power(n):
+    global bridge_length, solution, cnt
+    if n >= bridge_length:
+        final = []
+        if sum(check) == 0:
+            return
+        for k in range(bridge_length):
+            if check[k] == 1:
+                final.append(bridge[k])
+        q = [final[0][0], final[0][1]]
+        result = [final[0][0], final[0][1]]
+        rresult = final[0][2]
+        while q:
+            new = q.pop(0)
+            for i in range(len(final)):
+                if final[i][0] == new or final[i][1] == new:
+                    if final[i][0] not in result:
+                        result.append(final[i][0])
+                        q.append(final[i][0])
+                        rresult += final[i][2]
+                    if final[i][1] not in result:
+                        result.append(final[i][1])
+                        q.append(final[i][1])
+                        rresult += final[i][2]
+        result = list(set(result))
+        # print(result)
+        if result == list(range(1, cnt)):
+            if solution >= rresult:
+                solution = rresult
+
+
+
+    else:
+        check[n] = 1
+        power(n+1)
+        check[n] = 0
+        power(n+1)
+
+
 Y, X = map(int, input().split())
 arr = [list(map(int, input().split())) for _ in range(Y)]
 narr = [[0] * X for _ in range(Y)]
@@ -24,4 +63,48 @@ for y in range(Y):
             island(y, x, cnt)
             cnt += 1
 
+dis_dict = {}
+for y in range(Y):
+    for x in range(X):
+        if narr[y][x] != 0:
+            for i in range(4):
+                ny = y + dy[i]
+                nx = x + dx[i]
+                ccnt = 0
+                while True:
+                    if ny < 0 or nx < 0 or ny >= Y or nx >= X:break
+                    if narr[ny][nx] == narr[y][x]:break
+                    if narr[ny][nx] != 0 and narr[ny][nx] != narr[y][x]:
+                        if ccnt >= 2:
+                            if (narr[ny][nx], narr[y][x]) in dis_dict:
+                                if dis_dict[(narr[ny][nx], narr[y][x])] > ccnt:
+                                    dis_dict[(narr[ny][nx], narr[y][x])] = ccnt
+                            elif (narr[y][x], narr[ny][nx]) in dis_dict:
+                                if dis_dict[(narr[y][x], narr[ny][nx])] > ccnt:
+                                    dis_dict[(narr[y][x], narr[ny][nx])] = ccnt
+                            else:
+                                dis_dict[(narr[ny][nx], narr[y][x])] = ccnt
+                            break
+                        else:
+                            break
+                    ccnt += 1
+                    ny += dy[i]
+                    nx += dx[i]
 
+
+bridge = []
+for key, value in dis_dict.items():
+    key = list(key)
+    key.append(value)
+    bridge.append(key)
+
+bridge_length = len(bridge)
+check = [0] * bridge_length
+
+solution = 987654321
+
+power(0)
+if solution == 987654321:
+    print(-1)
+else:
+    print(solution)
