6fb5fb39fe16b3abc14a99d2c1c5aa6df5ed22b5
sochic2 <netzzang12@gmail.com>
2020-01-14 15:38:59 +0900

    프로그래머스 스킬테스트3 통과

---
+++ algorithm/2020_01/죠르디.py
@@ -0,0 +1,47 @@
+n = 5
+build_frame = 	[[0,0,0,1],[2,0,0,1],[4,0,0,1],[0,1,1,1],[1,1,1,1],[2,1,1,1],[3,1,1,1],[2,0,0,0],[1,1,1,0],[2,2,0,1]]
+
+def solution(n, build_frame):
+    answer = [[]]
+    result = []
+    for frame in build_frame:
+        x, y, structure, build = frame[0], frame[1], frame[2], frame[3]
+        # 삭제
+        if build == 0:
+            result.remove([x, y, structure])
+            true_false = check(result)
+            if true_false:
+                continue
+            else:
+                result.append([x, y, structure])
+
+
+        # 설치
+        else:
+            result.append([x, y, structure])
+            true_false = check(result)
+            if true_false:
+                continue
+            else:
+                result.pop()
+    result.sort()
+    return result
+
+
+
+def check(result):
+    for re in result:
+        x, y, structure = re[0], re[1], re[2]
+        # 기둥
+        if structure == 0:
+            if y == 0 or [x, y-1, 0] in result or [x, y, 1] in result or [x-1, y, 1] in result: continue
+            else: return False
+        # 보
+        else:
+            if ([x-1, y , 1] in result and [x+1, y, 1] in result) or [x, y-1, 0] in result or [x+1, y-1, 0] in result: continue
+            else: return False
+
+
+    return True
+
+print(solution(n, build_frame))
