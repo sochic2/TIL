0458f3aba9fadd5b432484e6769a733f0c79113d
sochic2 <netzzang12@gmail.com>
2020-02-04 16:19:42 +0900

    원판돌리기

---
+++ algorithm/2020_02/원판돌리기.py
@@ -0,0 +1,6 @@
+N, M, T = map(int, input().split())
+plate = [list(map(int, input().split())) for _ in range(N)]
+change = [list(map(int, input().split())) for _ in range(T)]
+
+print(plate)
+print(change)
