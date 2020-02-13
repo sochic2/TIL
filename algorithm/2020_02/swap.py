9828d98ef9c735fd4f2c08fd6bff1c12c24d022c
sochic2 <netzzang12@gmail.com>
2020-02-13 21:40:11 +0900

    swql2

--- algorithm/2020_02/swap.py
+++ algorithm/2020_02/swap.py
@@ -6,5 +6,6 @@ numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 numbers[3], numbers[numbers.index(max(numbers[3:]))] = numbers[numbers.index(max(numbers[3:]))], numbers[3]
 
 
+
 print(type(numbers.index(max(numbers[3:]))))
 print(numbers.index(max(numbers[3:])))
